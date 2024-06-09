91226694969161ca94cbaba7c6b1358d45f1ec1b
DouglasOGarrido <douglasogarrido@gmail.com>
2024-06-08 23:37:08 -0300

    Ajustes para consultar dados do tempo

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,12 +12,21 @@ export class WeatherService {
   private apiKey = '89c5b1b153068d744b97485c9a020ad9';
   private apiURL = `api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=${this.apiKey}'`;
 
+  private baseURL = 'api.openweathermap.org/data/2.5/forecast/daily';
 
 
   constructor(private http:HttpClient) {}
 
-  getWeatherData(): Observable<any>{
-    return this.http.get(this.apiURL);
+  // cnt param: A number of days, which will be returned 
+  // in the API response (from 1 to 16)
+  getWeatherData(cityName: string, cnt: number = 16): Observable<any>{
+    return this.http.get(this.baseURL, {
+      params: {
+        'q': cityName,
+        'cnt': cnt,
+        'appid': this.apiKey,
+      }
+    });
   }
 
 }
